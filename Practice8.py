57ba4bd82248014894be94eadb0c3afd60f41bb3
TeodorCross <100580658+TeodorCross@users.noreply.github.com>
2022-04-29 19:25:53 +0500

    Add files via upload

---
+++ Practice8.py
@@ -0,0 +1,65 @@
+#Task1
+
+import re
+EMAIL = re.compile(r'([a-z0-9_\.]+)@([f\a-z0-9]+\.[a-z]+)')
+def email_parse(email):
+    found_info = EMAIL.findall(email)
+    if found_info:
+        name, addr = found_info[0]
+    else:
+        raise ValueError(f'wrong email:{email}')
+    print(name, addr)
+
+email_parse('ted200798@mail.ru')
+email_parse('ted200798@mailru')
+
+#Task3
+
+from functools import wraps
+def type_logger(func):
+    @wraps(func)
+    def wrapper(*args):
+        for arg in args:
+            print(f'{func.__name__}({arg}: {type(arg)})' , end=',')
+        return func(*args)
+
+    return wrapper
+
+
+@type_logger
+def calc_cube(*args):
+    """this shows only with 'from functools import wraps'"""
+    return list(map(lambda x: x ** 3, args))
+
+a = calc_cube(5, 3)
+print(a)
+print(calc_cube.__name__)
+print(calc_cube.__doc__)
+
+#Task4
+
+from functools import wraps
+
+
+def val_checker(decorator_check_finc):
+    def _val_checker(func_calc_cube):
+        @wraps(func_calc_cube)
+        def wrapped(x):
+            if decorator_check_finc(x):
+                return func_calc_cube(x)
+            else:
+                raise ValueError(x)
+
+        return wrapped
+    return _val_checker
+
+
+@val_checker(lambda x: x > 0)
+def calc_cube(x):
+    """calc_cube desc"""
+    return x ** 3
+
+a = calc_cube(5)
+print(a)
+print(calc_cube.__name__)
+print(calc_cube.__doc__)
